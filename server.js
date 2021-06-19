94e0516b767489240adb2f8178a758b821695ae8
Mazhar Hussain <maxhar30@gmail.com>
2021-06-19 13:47:44 +0500

    Server Added

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 const app = express();
 
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080
+
+//log requests
+app.use(morgan('tiny'));
+
+//parse Requst to body-paser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engin
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname,"views/ejs"));
+
+//load Assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+
+
 app.get('/', (req, res) => {
-    res.send("We Are in Homme");
+    res.render('index');
 })
 
-app.listen(3000, () => {
-    console.log('Server is Running on Port : 3000');
+app.listen(PORT, () => {
+    console.log(`Server is Running on Port :${PORT}`);
 })
